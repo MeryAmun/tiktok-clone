9d63627e7046a23c814462cff987c05a45c68344
MeryAmun <chebesiawah@gmail.com>
2023-02-06 03:52:44 +0100

    side bar done

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ function App() {
   return (
     <div className="app">
      <div className="app__videos">
-      <Video/>
-      <Video/>
-      <Video/>
-      <Video/>
+      <Video url="https://v16-webapp.tiktok.com/a6b405130db066385ee6a70c9fcc14bf/63e09ffb/video/tos/maliva/tos-maliva-ve-0068c799-us/8033d7da636548429d70afd9a4807f52/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=7802&bt=3901&cs=0&ds=3&ft=-ElcommJPD12NJDlX8-UxItFhYKt3wv25IcAV&mime_type=video_mp4&qs=0&rc=N2llaDk6OzY0ZDVnM2c1Z0BpajU3Nzs6ZmhnaDMzZzczNEA0YTRhM15jNjQxYmIyNjBhYSM0anFqcjQwXjNgLS1kMS9zcw%3D%3D&l=202302060036330164F50174A796CF4D5C&btag=80000"
+     channel='Chebs007' messages='this and that' shares={12} description='The One' song='Oceans ' likes={18}
+      />
+      <Video url="https://v16-webapp.tiktok.com/a6b405130db066385ee6a70c9fcc14bf/63e09ffb/video/tos/maliva/tos-maliva-ve-0068c799-us/8033d7da636548429d70afd9a4807f52/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=7802&bt=3901&cs=0&ds=3&ft=-ElcommJPD12NJDlX8-UxItFhYKt3wv25IcAV&mime_type=video_mp4&qs=0&rc=N2llaDk6OzY0ZDVnM2c1Z0BpajU3Nzs6ZmhnaDMzZzczNEA0YTRhM15jNjQxYmIyNjBhYSM0anFqcjQwXjNgLS1kMS9zcw%3D%3D&l=202302060036330164F50174A796CF4D5C&btag=80000"
+     channel='Chebs007' messages='this and that' shares={12} description='The One' song='Oceans ' likes={18}
+      />
+      <Video url="https://v16-webapp.tiktok.com/a6b405130db066385ee6a70c9fcc14bf/63e09ffb/video/tos/maliva/tos-maliva-ve-0068c799-us/8033d7da636548429d70afd9a4807f52/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=7802&bt=3901&cs=0&ds=3&ft=-ElcommJPD12NJDlX8-UxItFhYKt3wv25IcAV&mime_type=video_mp4&qs=0&rc=N2llaDk6OzY0ZDVnM2c1Z0BpajU3Nzs6ZmhnaDMzZzczNEA0YTRhM15jNjQxYmIyNjBhYSM0anFqcjQwXjNgLS1kMS9zcw%3D%3D&l=202302060036330164F50174A796CF4D5C&btag=80000"
+     channel='Chebs007' messages='this and that' shares={12} description='The One' song='Oceans ' likes={18}
+      />
+      <Video url="https://v16-webapp.tiktok.com/a6b405130db066385ee6a70c9fcc14bf/63e09ffb/video/tos/maliva/tos-maliva-ve-0068c799-us/8033d7da636548429d70afd9a4807f52/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=7802&bt=3901&cs=0&ds=3&ft=-ElcommJPD12NJDlX8-UxItFhYKt3wv25IcAV&mime_type=video_mp4&qs=0&rc=N2llaDk6OzY0ZDVnM2c1Z0BpajU3Nzs6ZmhnaDMzZzczNEA0YTRhM15jNjQxYmIyNjBhYSM0anFqcjQwXjNgLS1kMS9zcw%3D%3D&l=202302060036330164F50174A796CF4D5C&btag=80000"
+     channel='Chebs007' messages={400} shares={120} description='The One' song='Oceans ' likes={2018}
+      />
      </div>
     </div>
   );
--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -3,7 +3,10 @@ import Footer from '../Footer/Footer';
 import Sidebar from '../sidebar/Sidebar';
 import './video.css'
 
-const Video = () => {
+
+
+
+const Video = ({url, channel, messages, shares, description,song, likes}) => {
     const [playing, setPlaying] = useState(false)
     const videoRef = useRef(null)
 
@@ -24,10 +27,10 @@ const Video = () => {
        loop
        onClick={onVideoPress}
        ref={videoRef}
-       src="https://v16-webapp.tiktok.com/a6b405130db066385ee6a70c9fcc14bf/63e09ffb/video/tos/maliva/tos-maliva-ve-0068c799-us/8033d7da636548429d70afd9a4807f52/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=7802&bt=3901&cs=0&ds=3&ft=-ElcommJPD12NJDlX8-UxItFhYKt3wv25IcAV&mime_type=video_mp4&qs=0&rc=N2llaDk6OzY0ZDVnM2c1Z0BpajU3Nzs6ZmhnaDMzZzczNEA0YTRhM15jNjQxYmIyNjBhYSM0anFqcjQwXjNgLS1kMS9zcw%3D%3D&l=202302060036330164F50174A796CF4D5C&btag=80000"
+       src={url}
        ></video>
-       <Sidebar likes={15} shares={6} comments={8}/>
-      <Footer channel='Chebs007' description='The One' song='Oceans'/>
+       <Sidebar likes={likes} shares={shares} messages={messages}/>
+      <Footer channel={channel} description={description} song={song}/>
     </div>
   )
 }
